fe00a8058453770e8d5d46f7a84bc85024dd030d
Sebastian Martinez <smariver@gmail.com>
2021-06-06 14:49:41 -0300

    Added UI, modified html css and js Working rock paper scissors game with working UI, plays the game, when one reaches score 5 it waits for 2 seconds and resets the counter again.

--- script.js
+++ script.js
@@ -1,83 +1,100 @@
+document.addEventListener("DOMContentLoaded", function(){
 
-//playerSelection = prompt("Choose a character: rock, paper, scissors");
+    let computerPoints = 0, humanPoints = 0;
+    
+    game();
 
-function roundFight(playerSelection, computerSelection){
-    playerSelection = playerSelection.toLowerCase();
-    switch(playerSelection){
-        case "scissors":
-            if(computerSelection === "scissors")
-                return "Its a tie!";
-            else
-                if(computerSelection === "rock")
-                    return "You lose!";
-                else
-                    if(computerSelection === "paper")
-                        return "You win!";
-            break;
-
-        case "rock":
-            if(computerSelection === "rock")
-                return "Its a tie!";
-            else
-                if(computerSelection === "paper")
-                    return "You lose!";
-                else
-                    if(computerSelection === "scissors")
-                        return "You win!";
-            break;
-
-        case "paper":
-            if(computerSelection === "paper")
-                return "Its a tie!";
-            else
+    function roundFight(playerSelection, computerSelection){
+        playerSelection = playerSelection.toLowerCase();
+        switch(playerSelection){
+            case "scissors":
                 if(computerSelection === "scissors")
-                    return "You lose!";
+                    return "Its a tie!";
                 else
                     if(computerSelection === "rock")
-                        return "You win!";
-            break;
-    }
-}
+                        return "You lose!";
+                    else
+                        if(computerSelection === "paper")
+                            return "You win!";
+                break;
 
-function computerPlay(){
-    let num=Math.random()*9;
-    if(num<3)
-        return "rock";
-    else
-        if(num>6)
-            return "paper";
-        else
-            return "scissors";
-}
+            case "rock":
+                if(computerSelection === "rock")
+                    return "Its a tie!";
+                else
+                    if(computerSelection === "paper")
+                        return "You lose!";
+                    else
+                        if(computerSelection === "scissors")
+                            return "You win!";
+                break;
 
-function game(){
-    let i, playerSel, roundResult, computerPoints = 0, humanPoints = 0;
-    for (i=0; i<5; i++){
-        playerSel = prompt("Please enter your play", "Play!")
-        roundResult = roundFight(playerSel, computerPlay());
-        if(roundResult === "Its a tie!"){
-            console.log(roundResult)
+            case "paper":
+                if(computerSelection === "paper")
+                    return "Its a tie!";
+                else
+                    if(computerSelection === "scissors")
+                        return "You lose!";
+                    else
+                        if(computerSelection === "rock")
+                            return "You win!";
+                break;
         }
-        else
-            if(roundResult === "You lose!"){
-                console.log(roundResult)
-                computerPoints++;
-            }
-            else{
-                console.log(roundResult)
-                humanPoints++;
-            }
-                
     }
-    if(computerPoints>humanPoints)
-        console.log("Computer wins!")
-    else
-        if(computerPoints<humanPoints)
-            console.log("Human wins!")
-        else
-            console.log("Its a tie, robots dont dominate us yet!")
-}
 
-/*const playerSelection = "rock";
-const computerSelection = computerPlay();
-console.log(roundFight(playerSelection, computerSelection));*/
+    function computerPlay(){
+        let num=Math.random()*9;
+        if(num<3)
+            return "rock";
+        else
+            if(num>6)
+                return "paper";
+            else
+                return "scissors";
+    }
+
+    function score(roundResult){
+        let leftScore = document.getElementById("leftScore");
+        let rightScore = document.getElementById("rightScore");
+
+        if(roundResult==="You win!")
+                humanPoints++;
+            else
+                if(roundResult==="You lose!")
+                    computerPoints++;
+        
+        
+
+        leftScore.innerText = humanPoints;
+        rightScore.innerText = computerPoints;
+
+        if(computerPoints === 5 || humanPoints === 5){
+            computerPoints = humanPoints = 0;
+            setTimeout(function(){leftScore.innerText = humanPoints; rightScore.innerText = computerPoints;}, 2000)
+        }
+    }
+
+    function game(){
+        let i, playerSel, roundResult;
+        
+        let rockButton = document.getElementById("rockButton");
+        let paperButton = document.getElementById("paperButton");
+        let scissorsButton = document.getElementById("scissorsButton");
+        
+        rockButton.addEventListener("click", function(){
+            playerSel = "rock";
+            roundResult=roundFight(playerSel, computerPlay());
+            score(roundResult);
+        });
+        paperButton.addEventListener("click", function(){
+            playerSel = "paper";
+            roundResult=roundFight(playerSel, computerPlay());
+            score(roundResult);
+        });
+        scissorsButton.addEventListener("click", function(){
+            playerSel = "scissors";
+            roundResult=roundFight(playerSel, computerPlay());
+            score(roundResult);
+        });
+    }
+});
